6edb32379019b70bd61f48f0d36df94e33af2251
Micah Stalberg <stalberm@oregonstate.edu>
2022-04-14 20:04:13 -0700

    Add case for line 5 of poem

--- lib/house.rb
+++ lib/house.rb
@@ -9,6 +9,8 @@ class House
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        when 5
+            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
