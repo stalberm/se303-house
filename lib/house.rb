0bd43862ed78a62ec1f7efddb97aedf6be61e434
Micah Stalberg <stalberm@oregonstate.edu>
2022-04-14 20:00:58 -0700

    Add elsif statement for line 3 of poem

--- lib/house.rb
+++ lib/house.rb
@@ -4,6 +4,8 @@ class House
             "This is the house that Jack built.\n"
         elsif number == 2
             "This is the malt that lay in the house that Jack built.\n"
+        elsif number == 3
+            "This is the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
