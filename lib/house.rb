8c0743563d31d54f8ab90918f36f2510add02de0
Micah Stalberg <stalberm@oregonstate.edu>
2022-04-14 20:03:14 -0700

    Change if elsif to case statement, add case for line 4 of poem

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,14 @@
 class House
     def line(number)
-        if number == 1
+        case number
+        when 1
             "This is the house that Jack built.\n"
-        elsif number == 2
+        when 2
             "This is the malt that lay in the house that Jack built.\n"
-        elsif number == 3
+        when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
+        when 4
+            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
