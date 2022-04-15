174e063c9a8c234ce86c2583ec776d02a3268dcd
Micah Stalberg <stalberm@oregonstate.edu>
2022-04-14 20:07:16 -0700

    Add case for line 8 of poem

--- lib/house.rb
+++ lib/house.rb
@@ -15,6 +15,8 @@ class House
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 7
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        when 8
+            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
