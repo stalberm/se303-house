b61c6ba6a0a217e66eb651766b25d607bd9bb731
Micah Stalberg <stalberm@oregonstate.edu>
2022-04-25 20:59:50 -0700

    Remove default value from difference2 method

--- lib/house.rb
+++ lib/house.rb
@@ -34,7 +34,7 @@ class House
         end
     end
     
-    def difference2(number = :FIXME)
+    def difference2(number)
         if number >= 11
             "the farmer sowing his corn that kept "
         else
