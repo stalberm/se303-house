4ae2ecbff9a8d6630327bf1139e31b0990ea4110
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-09 15:42:45 -0700

    Remove case 6

--- lib/house.rb
+++ lib/house.rb
@@ -11,8 +11,6 @@ class House
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 5
             "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 6
-            "This is #{difference(number)}#{difference2(number)}#{difference3(number)}#{difference4(number)}#{difference5(number)}#{difference6(number)}the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
             "This is #{difference(number)}#{difference2(number)}#{difference3(number)}#{difference4(number)}#{difference5(number)}#{difference6(number)}the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
