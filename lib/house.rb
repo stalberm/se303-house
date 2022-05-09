875b8c1cab3e4fd4e2a64593447078c7b212bf34
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-09 15:29:38 -0700

    Remove case 8

--- lib/house.rb
+++ lib/house.rb
@@ -15,8 +15,6 @@ class House
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 7
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 8
-            "This is #{difference(number)}#{difference2(number)}#{difference3(number)}#{difference4(number)}the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
             "This is #{difference(number)}#{difference2(number)}#{difference3(number)}#{difference4(number)}the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
