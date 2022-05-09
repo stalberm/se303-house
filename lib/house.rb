5533f8e7dd78424b5b4fa986075f5468af505e04
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-09 15:17:51 -0700

    Replace line difference in else block with difference4 method call

--- lib/house.rb
+++ lib/house.rb
@@ -18,7 +18,7 @@ class House
         when 8
             "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
-            "This is #{difference(number)}#{difference2(number)}#{difference3(number)}the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{difference(number)}#{difference2(number)}#{difference3(number)}#{difference4}the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 
