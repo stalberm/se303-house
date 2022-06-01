2edbc15d1e67076a5a6c797326523a3ebc97d2f7
Micah Stalberg <stalberm@oregonstate.edu>
2022-06-01 16:47:49 -0700

    Remove middleman difference call from lyrics method array

--- lib/house.rb
+++ lib/house.rb
@@ -13,7 +13,7 @@ class House
          "the man all tattered and torn that kissed ", 
          "the priest all shaven and shorn that married ", 
          "the rooster that crowed in the morn that woke ", 
-         "the farmer sowing his corn that kept ", difference(number)]
+         "the farmer sowing his corn that kept ", "the horse and the hound and the horn that belonged to "]
     end
 
     def difference(number)
