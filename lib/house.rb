883e1d345d24945fc26dd43ea6c7761e29d5e94d
Micah Stalberg <stalberm@oregonstate.edu>
2022-06-01 18:29:50 -0700

    Remove lyrics and prefix method from House class

--- lib/house.rb
+++ lib/house.rb
@@ -10,25 +10,6 @@ class House
         verse_template.new.line(number)
     end
 
-    def lyrics
-        ["the house that Jack built.",
-         "the malt that lay in ",
-         "the rat that ate ", 
-         "the cat that killed ", 
-         "the dog that worried ", 
-         "the cow with the crumpled horn that tossed ", 
-         "the maiden all forlorn that milked ", 
-         "the man all tattered and torn that kissed ", 
-         "the priest all shaven and shorn that married ", 
-         "the rooster that crowed in the morn that woke ", 
-         "the farmer sowing his corn that kept ",
-         "the horse and the hound and the horn that belonged to "]
-    end
-
-    def prefix
-        "This is "
-    end
-
     def recite
         1.upto(12).map{ |number| line(number)}.join("\n")
     end
