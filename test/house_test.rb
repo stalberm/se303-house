27b5f771ce13c610d32d11f6958918aef6ff5851
Micah Stalberg <stalberm@oregonstate.edu>
2022-06-01 18:03:56 -0700

    Unskip test_pirate_line_11

--- test/house_test.rb
+++ test/house_test.rb
@@ -145,7 +145,6 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
   end
 
   def test_pirate_line_11
-    skip
     expected = "Thar be the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, House.new(PirateVerse).line(11)
   end
