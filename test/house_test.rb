e5bd2a87872bd361c26a6e88ebe872fa397deaab
Micah Stalberg <stalberm@oregonstate.edu>
2022-06-01 17:39:11 -0700

    Add line 1 test for pirate version

--- test/house_test.rb
+++ test/house_test.rb
@@ -94,4 +94,9 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
     assert_equal expected, House.new.recite
   end
 
+  def test_pirate_line_1
+    expected = "Thar be the house that Jack built.\n"
+    assert_equal expected, House.new(PirateVerse).line(1)
+  end
+
 end
