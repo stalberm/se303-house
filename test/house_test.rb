319e3016b0d2ccdc832794851c8764f1824caa6e
Micah Stalberg <stalberm@oregonstate.edu>
2022-06-03 10:04:52 -0700

    Unskip test_random_noun_verb_line_3

--- test/house_test.rb
+++ test/house_test.rb
@@ -372,7 +372,6 @@ Thar be the priest all shaven and shorn that married the dog that worried the ma
   end
 
   def test_random_noun_verb_line_3
-    skip
     expected = "This is the rat that lay in the man all tattered and torn that married the house that Jack built.\n"
     assert_equal expected, House.new(RandomNounVerb).line(3)
   end
